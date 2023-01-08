42ca338f661bd89369a3bb7f163f9bfa01dc7d92
esg-eduard@mail.ru <esg-eduard@mail.ru>
2023-01-08 22:50:36 +0300

    Добавьте файлы проекта.

---
+++ Wpf-Task19/Models/LengthCircle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Task19.Models
+{
+    static class LengthCircle
+    {
+        public static double Length(double a) => Math.PI * 2 * a;
+    }
+}
